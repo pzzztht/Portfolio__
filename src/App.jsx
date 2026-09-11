0eb31360c59995c81e15ed42d639a00e9e191110
Phoo <pthtftzaw@gmail.com>
2026-09-11 12:59:22 +0630

    Navbar and hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { Navbar } from "@/sections/navbar";
-import { Hero } from "@/sections/hero";
-import { About } from "./sections/about";
+import { Navbar } from "@/layout/Navbar";
+import { Hero } from "@/sections/Hero";
+import { About } from "@/sections/About";
 import { Projects } from "@/sections/Projects";
-import { Experience } from "./sections/experience";
-import { Testimonials } from "./sections/testimonials";
-import { Contact } from "./sections/contact";
+import { Experience } from "@/sections/Experience";
+import { Testimonials } from "@/sections/Testimonials";
+import { Contact } from "@/sections/Contact";
 
 function App() {
   return (
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,3 +1,11 @@
 export const Hero = () => {
-    return <section>Hello</section>;
+ return ( 
+    <section className="relative min-h-screen flex items-center overflow-hidden">
+      {/* Bg */}
+        <div className="absolute inset-0">
+          <img src="/hero-bg.jpg" alt="Hero image" 
+          className="w-full h-full object-cover opacity-40"/>
+        </div>
+    </section>
+  );
 }
